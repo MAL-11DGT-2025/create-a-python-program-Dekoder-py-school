441d07912e10500a6d8ad460eb487625b236a6f4
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-03-04 10:26:55 +1300

    add .title() to display name with proper capitalisation

--- hello.py
+++ hello.py
@@ -5,5 +5,5 @@ user_name = input(
     "What is your name? "
 )  # ask the user for their name and save it in user_name
 print(
-    f"Hello, {user_name or 'friend'}!"
+    f"Hello, {user_name.title() or 'friend'}!"
 )  # greet the user using their name or friend if user_name is empty

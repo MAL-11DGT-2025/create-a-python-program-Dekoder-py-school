4e8034f47a692358c542f16193516af2059adfe8
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-03-04 10:36:21 +1300

    add age input with error handling

--- hello.py
+++ hello.py
@@ -7,3 +7,18 @@ user_name = input(
 print(
     f"Hello, {user_name.title() or 'friend'}!"
 )  # greet the user using their name or friend if user_name is empty
+
+while True:
+    user_age_string = input("Enter your age (in years): ")
+    # try to convert the user's age input to an int, if it fails try again.
+    try:
+        user_age = int(user_age_string)
+        break
+    except ValueError:  # meaning the user has typed something that cannot be a number
+        print("Please enter only numbers!")
+        continue
+
+if user_age >= 55:
+    print(f"Wow! {user_age} is pretty old!")
+else:
+    print(f"{user_age} is pretty young!")

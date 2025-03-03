4e09d59c091a49e49306eee1ed2b37a0c4faafb8
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-03-04 10:26:11 +1300

    add comments to explain the code

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 print("Hello, world!")
 print("My favorite color is blue.")
 
-user_name = input("What is your name? ")
-print(f"Hello, {user_name}!")
+user_name = input(
+    "What is your name? "
+)  # ask the user for their name and save it in user_name
+print(
+    f"Hello, {user_name or 'friend'}!"
+)  # greet the user using their name or friend if user_name is empty

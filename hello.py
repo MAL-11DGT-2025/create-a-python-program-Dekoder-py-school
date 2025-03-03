8b8ddf5d1a59e1ca373eabeb46203f514d8c93e1
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-03-04 10:38:34 +1300

    re format

--- hello.py
+++ hello.py
@@ -1,12 +1,8 @@
 # print("Hello, world!")
 # print("My favorite color is blue.")
 
-user_name = input(
-    "What is your name? "
-)  # ask the user for their name and save it in user_name
-print(
-    f"Hello, {user_name.title() or 'friend'}!"
-)  # greet the user using their name or friend if user_name is empty
+user_name = input("What is your name? ")  # ask the user for their name and save it in user_name
+print(f"Hello, {user_name.title() or 'friend'}!")  # greet the user using their name or friend if user_name is empty
 
 while True:
     user_age_string = input("Enter your age (in years): ")

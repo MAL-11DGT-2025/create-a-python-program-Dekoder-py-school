a17bc1eeb7f8d742e90b6b7511e5e45297a3a42c
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-03-06 14:32:02 +1300

    comment out all code

--- hello.py
+++ hello.py
@@ -1,25 +1,28 @@
 # print("Hello, world!")
 # print("My favorite color is blue.")
 
-user_name = input(
-    "What is your name? "
-)  # ask the user for their name and save it in user_name
-print(
-    f"Hello, {user_name.title() or 'friend'}!"
-)  # greet the user using their name or friend if user_name is empty
+# user_name = input(
+#     "What is your name? "
+# )  # ask the user for their name and save it in user_name
+# print(
+#     f"Hello, {user_name.title() or 'friend'}!"
+# )  # greet the user using their name or friend if user_name is empty
 
-while True:
-    user_age_string = input("Enter your age (in years): ")
-    # try to convert the user's age input to an int, if it fails try again.
-    try:
-        user_age = int(user_age_string)
-        break
-    except ValueError:  # meaning the user has typed something that cannot be a number
-        print("Please enter only numbers!")
-        continue
+# while True:
+#     user_age_string = input("Enter your age (in years): ")
+#     # try to convert the user's age input to an int, if it fails try again.
+#     try:
+#         user_age = int(user_age_string)
+#         break
+#     except ValueError:  # meaning the user has typed something that cannot be a number
+#         print("Please enter only numbers!")
+#         continue
 
-if user_age >= 25:
-    print(f"Wow! {user_age} is pretty old!")
+# if user_age >= 25:
+#     print(f"Wow! {user_age} is pretty old!")
 
-if user_age >= 13 and user_age <= 19:
-    print("You are a teenager!")
+# if user_age >= 13 and user_age <= 19:
+#     print("You are a teenager!")
+
+# if user_age >= 13 or user_age <= 19:
+#     print("You might be a teenager!")

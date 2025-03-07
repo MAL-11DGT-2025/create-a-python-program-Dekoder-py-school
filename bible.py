4cb0bf299626dbb6c8905381aaf34beac275d50c
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-03-07 13:12:06 +1300

    create bible quiz and write first question

---
+++ bible.py
@@ -0,0 +1,21 @@
+book = (
+    input("""What is the first book of the bible? 
+a) Genesis
+b) Exodus
+c) Numbers
+d) Leviticus
+>> """)
+    .lower()
+    .strip()
+)
+
+
+options = ["a", "genesis", "b", "exodus", "c", "numbers", "d", "leviticus"]
+
+if book in options:
+    if book == "a" or book == "genesis":
+        print("Correct!")
+    else:
+        print("Incorrect.")
+else:
+    print("Invalid option.")

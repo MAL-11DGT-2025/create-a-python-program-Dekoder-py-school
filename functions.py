6fb9e8ead8a69825e00a783fedab0a97fc9a3464
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-03-13 10:52:49 +1300

    create subtraction function

--- functions.py
+++ functions.py
@@ -12,7 +12,13 @@ def personal_greeting(name: str) -> None:
 
 
 def add(a: int, b: int) -> None:
+    """Add two numbers together and print the result"""
     print(a + b)
+
+
+def subtract(a: int, b: int) -> None:
+    """Subtract one number from the other and print the result"""
+    print(a - b)
 
 
 # user_name: str = input("What is your name: ").title()
@@ -20,5 +26,8 @@ def add(a: int, b: int) -> None:
 # greeting()
 # personal_greeting(user_name)
 add(5, 10)
-add(23, 104)
-add(55, 1120)
+add(1, 2)
+add(2, 3)
+subtract(10, 12)
+subtract(5, 3)
+subtract(12, 12)

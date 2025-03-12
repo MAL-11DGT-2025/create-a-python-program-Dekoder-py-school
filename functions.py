9b52794b4b782b01e69e6f6cd77402d6930c109c
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-03-13 10:23:18 +1300

    create a function to greet the user by name

--- functions.py
+++ functions.py
@@ -1,6 +1,16 @@
+"""Python program to practice using functions"""
+
+
 def greeting() -> None:
     """Prints a friendly greeting for the user"""
     print("Hello!")
 
 
+def greeting_with_name(name) -> None:
+    print(f"Hey, {name}!")
+
+
+user_name: str = input("What is your name: ").title()
+
 greeting()
+greeting_with_name(user_name)

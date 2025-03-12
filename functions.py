2dcbcbad4f62fd673ae12b7221b6ca6be9f1e9fb
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-03-13 10:29:07 +1300

    create additon function, add type hinting, comment out user_name code

--- functions.py
+++ functions.py
@@ -6,12 +6,19 @@ def greeting() -> None:
     print("Hello!")
 
 
-def personal_greeting(name) -> None:
+def personal_greeting(name: str) -> None:
     """Prints a personalized greeting for the user"""
     print(f"Hey, {name}!")
 
 
-user_name: str = input("What is your name: ").title()
+def add(a: int, b: int) -> None:
+    print(a + b)
+
+
+# user_name: str = input("What is your name: ").title()
 
 # greeting()
-personal_greeting(user_name)
+# personal_greeting(user_name)
+add(5, 10)
+add(23, 104)
+add(55, 1120)

166c3447642f3a56c9cea9bf6e30898768dae330
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-03-26 09:16:09 +1300

    create a subtraction function

--- simple_calculate.py
+++ simple_calculate.py
@@ -2,9 +2,15 @@ def add(a: int, b: int) -> None:
     print(a + b)
 
 
-operation = input("Enter a operator (+, -, *, /): ")
+def subtract(a: int, b: int) -> None:
+    print(a - b)
+
+
+operation = input("Enter a operator (+, -, *, /): ").strip()
 a = int(input("First number: "))
 b = int(input("Second number: "))
 
 if operation == "+":
     add(a, b)
+elif operation == "-":
+    subtract(a, b)
